0ca360437d2209b7ed79535411a7ada46f177dfb
jairpro <jairedias@yahoo.com.br>
2020-09-21 19:14:13 -0300

    Colocar email fictício inválido na migration #21
    Colocar email fictício inválido na migration #21

--- src/database/seeds/20200914210000-dev-admin.sql
+++ src/database/seeds/20200914210000-dev-admin.sql
@@ -4,5 +4,5 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 INSERT INTO `admins` (`email`, `hash`, `name`, `user`, `type`, `status`) VALUES
-('devl@example.com', '$2y$12$UddsCtloH9PcNjdbc93/2uBjgSVVm5fRJoiIMecc6US018SpuTSvi', 'Developer', 'dev', 'D', 'A');
+('devl@example', '$2y$12$UddsCtloH9PcNjdbc93/2uBjgSVVm5fRJoiIMecc6US018SpuTSvi', 'Developer', 'dev', 'D', 'A');
 COMMIT;
